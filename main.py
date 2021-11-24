28a17d1305398ff4961d26665c7132150e891729
HumanBean17 <doudmitry@gmail.com>
2021-11-24 18:30:27 +0300

    ok

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 # coding=utf-8
-import io
+import Tkinter as tk
+from tkinter import font as tkFont
 import sys
 
 
 global k
 k = 1
+
 
 # Terminals
 EQUALS = "="
@@ -36,8 +38,8 @@ def get_next_token(tokens, number):
         try:
             result = tokens.pop(0)
         except IndexError:
-            print("<конец строки>")
-            sys.exit(0)
+            err_msg = "<конец строки>"
+            raise Exception(err_msg)
         dump.append(result)
     else:
         result = tokens[:number]
@@ -116,10 +118,10 @@ def operator(tokens, next_token):
             next_token = get_next_token(tokens, 1)
         if is_var(next_token) is False:
             if is_metka is True:
-                err_msg = "Ошибка при обработке Оператора. " + next_token + " . После метки \':\' ожидалась переменная"
+                err_msg = "Ошибка при обработке Оператора. После Метки \':\' ожидалась переменная. Получено: \'" + next_token + "\'"
                 raise Exception(err_msg)
             else:
-                err_msg = "Ошибка при обработке Оператора. Получен \'" + next_token + "\' . Ожидалась переменная"
+                err_msg = "Ошибка при обработке Оператора. Ожидалась переменная. Получено \'" + next_token + "\'"
                 raise Exception(err_msg)
         next_token = get_next_token(tokens, 1)
         if next_token != EQUALS:
@@ -213,13 +215,64 @@ def _set(tokens, next_token):
             next_token = get_next_token(tokens, 1)
 
 
+def start_parse():
+    user_input = text_edit.get(0.1, tk.END).decode().split()
+    try:
+        nt = definition(user_input)
+        nt = operator(user_input, nt)
+        _set(user_input, nt)
+    except Exception as e:
+        print(e.message)
+        output.configure(state=tk.NORMAL)
+        output.delete(0.1, tk.END)
+        output.insert(0.1, e.message)
+        output.configure(state=tk.DISABLED)
+        # v.set(e.message)
+        return e.message
+
+
 if __name__ == "__main__":
     reload(sys)
     sys.setdefaultencoding('utf-8')
 
-    s = "\"Метки\" 5.67 ; 1.22 ; 5.67 ; 1.22 \"Метки\" 5.67 ; 1.22 ; 5.67 ; 1.22 567 : А13121 = 5.1 + ( ( 4.1 + 1.1 ) + 3.1 + 2.7 + [ [ 1.0 / 1.2 ] ] ) А13121 = 5.1 + ( ( 4.1 + 1.1 ) + 3.1 + 2.7 + [ [ 1.0 / 1.2 ] ] ) \"Анализ\" 233 , 899 , 123 , 105 \"Анализ\" 100 , 100"
-    user_input = s.decode().split()
+    root = tk.Tk()
+    root.resizable(False, False)
+    root.title("BNF")
+    root.geometry("1200x700")
 
-    nt = definition(user_input)
-    nt = operator(user_input, nt)
-    _set(user_input, nt)
+    helv36 = tkFont.Font(family='Courier New', size=14)
+    text_edit = tk.Text(root, borderwidth=0.5, wrap=tk.WORD, relief=tk.SOLID)
+    text_edit['font'] = helv36
+    text_edit.place(relx=0.26, rely=0.37, anchor="c", height=500, width=600, bordermode=tk.OUTSIDE)
+
+    bnf = "Язык = Определение … Определение Опер … Опер Множество … Множество\n" \
+          "Определение = «Метки» Вещ “;” … Вещ\n" \
+          "Опер = </ Метка “:”/> Перемен. “=” Прав. Часть\n" \
+          "Множество = «Анализ» Цел “,” … Цел\n" \
+          "Прав_Часть = </“-“/> Блок_1 Знак_1 … Блок_1\n" \
+          "Знак_1 = “+” ! “-“\n" \
+          "Блок_1 = Блок_2 Знак_2 … Блок_2\n" \
+          "Знак_2 = “*” ! “/”\n" \
+          "Блок_2 = Блок_3 “^” … Блок_3\n" \
+          "Блок_3 = Перемен. ! Вещ. ! “(“ Прав_Часть “)” ! “[“ Прав_Часть “]” вложенность = 2\n" \
+          "Метка = Цел\n" \
+          "Перемен. = Буква </ Буква!Цифра … Буква!Цифра/>\n" \
+          "Вещ. = Цел “.” Цел\n" \
+          "Цел = Цифра…Цифра\n" \
+          "Буква = “А” ! “Б” ! … “Я”\n" \
+          "Цифра = “0” ! “!” … “F”"
+    label = tk.Label(root, text=bnf, font="14", justify=tk.LEFT)
+    label.place(relx=0.53, rely=0.039)
+
+    btn = tk.Button(root, text="Run", command=start_parse)
+    btn.place(relx=0.7, rely=0.5, width=200)
+
+    # v = tk.StringVar()
+    output = tk.Text(root, borderwidth=0.5, state=tk.DISABLED, relief=tk.SOLID)
+    output.place(relx=0.013, rely=0.8, height=100, width=1150, bordermode=tk.OUTSIDE)
+
+    root.mainloop()
+
+    # инфо о русских буквах
+
+
