e9b1b5e642dd4acd8a2a9b6a5777fabeb82e322d
HumanBean17 <doudmitry@gmail.com>
2021-12-12 19:35:19 +0300

    ok

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ SEMICOLON = ";"
 COLON = ":"
 COMMA = ","
 
-DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D", "E", "F"]
+DIGITS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 LITERALS = ["А", "Б", "В", "Г", "Д", "Е", "Ё", "Ж", "З", "И", "Й", "К", "Л", "М", "Н", "О", "П", "Р", "С", "Т", "У", "Ф", "Х", "Ц", "Ч", "Ш", "Щ", "Ъ", "Ы", "Ь", "Э", "Ю", "Я", "а", "б", "в", "г", "д", "е", "ё", "ж", "з", "и", "й", "к", "л", "м", "н", "о", "п", "р", "с", "т", "у", "ф", "х", "ц", "ч", "ш", "щ", "ъ", "ы", "ь", "э", "ю", "я"]
 
 TOKENS = [EQUALS, MINUS, PLUS, LEFT_ROUND_BRACKET, RIGHT_ROUND_BRACKET, LEFT_ROUND_BRACKET, RIGHT_SQUARE_BRACKET, DEGREE, MULT, DIV, METKI, SEMICOLON, COLON, COMMA] + DIGITS + LITERALS
